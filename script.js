7ec86ec947ca3f0f06e6135b08139b2853f9d73f
nnykaza <144247811+nnykaza@users.noreply.github.com>
2024-10-07 06:28:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function rollDice() {
+    for (let i = 1; i <= 5; i++) {
+        const dieValue = Math.floor(Math.random() * 6) + 1;
+        document.getElementById(`die${i}`).value = dieValue;
+    }
+}
